1d9b3abe2c596f949351eb57f1ef513e512c33f7
Yorick van Pelt <yorick@yorickvanpelt.nl>
2016-12-08 15:33:35 +0100

    remove ospinio

--- network.nix
+++ network.nix
@@ -1,9 +1,5 @@
 with (import <secrets>).hostnames; {
 	network.description = "yorick's stuff";
-	ospinio = {
-		imports = [./ospinio/configuration.nix];
-		deployment.targetHost = ospinio;
-	};
 	frumar = {
 		imports = [./frumar/configuration.nix];
 		deployment.targetHost = frumar;
