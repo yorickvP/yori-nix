cdcdd8d4a75d619d2ff84430fe5efa03564cd470
Yorick van Pelt <yorick@yorickvanpelt.nl>
2017-01-30 17:41:54 +0100

    add tmp pad hosting

--- pennyworth/configuration.nix
+++ pennyworth/configuration.nix
@@ -100,7 +100,13 @@ in
           systemctl restart prosody.service
       '';
     };
-    '';
+  services.nginx.virtualHosts."pad.yori.cc" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:9140";
+    };
+  };
 
   # hidden SSH service
 
