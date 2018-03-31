8b222d648acc38fa41b62b6b610875baaf080609
Yorick van Pelt <yorick@yorickvanpelt.nl>
2018-03-31 02:42:09 +0200

    add skl binary cache

--- logical/ascanius.nix
+++ logical/ascanius.nix
@@ -8,4 +8,12 @@
 
   system.stateVersion = "17.09";
 
+
+  nix.binaryCaches = [
+    "https://cache.nixos.org"
+    "https://builder.serokell.review"
+  ];
+  nix.binaryCachePublicKeys = [
+    "serokell:ic/49yTkeFIk4EBX1CZ/Wlt5fQfV7yCifaJyoM+S3Ss="
+  ];
 }
