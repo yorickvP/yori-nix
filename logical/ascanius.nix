c340dd24f3e22826207b076490c7ee0d6b480053
Yorick van Pelt <yorick@yorickvanpelt.nl>
2018-05-04 14:28:08 +0200

    add serokell cache

--- logical/ascanius.nix
+++ logical/ascanius.nix
@@ -9,11 +9,15 @@
   system.stateVersion = "17.09";
 
 
-  nix.binaryCaches = [
-    "https://cache.nixos.org"
-    "https://builder.serokell.review"
-  ];
-  nix.binaryCachePublicKeys = [
-    "serokell:ic/49yTkeFIk4EBX1CZ/Wlt5fQfV7yCifaJyoM+S3Ss="
-  ];
+  nix = {
+    binaryCaches = [
+      "https://cache.nixos.org"
+    ];
+    trustedBinaryCaches = config.nix.binaryCaches ++ [
+      "https://builder.serokell.io"
+    ];
+    binaryCachePublicKeys = [
+      "serokell:ic/49yTkeFIk4EBX1CZ/Wlt5fQfV7yCifaJyoM+S3Ss="
+    ];
+  };
 }
